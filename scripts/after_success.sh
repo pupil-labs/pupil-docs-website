73077e274cab67037190c58173e956f602f35052
Will Patera <willpatera@gmail.com>
2017-03-06 14:16:16 +0700

    test travis

--- scripts/after_success.sh
+++ scripts/after_success.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-    echo 'Setting up deployment keys...'
+    echo 'Setting up deployment keys......'
     # echo -e "Host ${DEPLOY_DOMAIN}\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config    
     eval "$(ssh-agent -s)"
     chmod 600 /tmp/docs_deploy_rsa
