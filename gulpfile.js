64c6204fbea0054effb90791627f58c0fbf8f983
Will Patera <willpatera@gmail.com>
2017-03-16 22:40:42 +0700

    updating javascript compile

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,8 @@ gulp.task("js:build:all", function(){
           SLATE_PATH+"javascripts/lib/_imagesloaded.min.js",
           SLATE_PATH+"javascripts/app/_lang.js",
           SLATE_PATH+"javascripts/app/_search.js",
-          SLATE_PATH+"javascripts/app/_toc.js"])
+          SLATE_PATH+"javascripts/app/_toc.js",
+          SLATE_PATH+"javascripts/app/_custom.js"])
           .pipe(concat('all.min.js'))
           .pipe(uglify())
           .pipe(gulp.dest(SLATE_PATH+"javascripts"))
@@ -84,7 +85,8 @@ gulp.task("js:build:all_nosearch", function(){
           SLATE_PATH+"javascripts/lib/_jquery.tocify.js",
           SLATE_PATH+"javascripts/lib/_imagesloaded.min.js",
           SLATE_PATH+"javascripts/app/_lang.js",
-          SLATE_PATH+"javascripts/app/_toc.js"])
+          SLATE_PATH+"javascripts/app/_toc.js",
+          SLATE_PATH+"javascripts/app/_custom.js"])
           .pipe(concat('all_nosearch.min.js'))
           .pipe(uglify())
           .pipe(gulp.dest(SLATE_PATH+"javascripts"))
