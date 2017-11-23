5fc370c2e56942a28fcaf30a61c3d7ecdfa7f992
nathakits <nathakit.sae-tan@cuinda.com>
2017-11-23 10:47:24 +0700

    update html proofer options

--- scripts/html_check.rb
+++ scripts/html_check.rb
@@ -1,7 +1,20 @@
 require 'html-proofer'
 require_relative 'custom_img_check'
 
-options = { :check_html => true, :checks_to_ignore => ["ImageCheck"], :file_ignore => [/.\/public.[^master].+/] }
+options = { 
+	:check_html => true, 
+	:checks_to_ignore => ["ImageCheck"],
+	:parallel => { :in_processes => 3},
+	:file_ignore => [/.\/public.[^master].+/],
+	:typhoeus => {
+		# avoid strange SSL errors: https://github.com/gjtorikian/html-proofer/issues/376
+		:connecttimeout => 20,
+		:timeout => 60,
+		:ssl_verifypeer => false,
+		:ssl_verifyhost => 0,
+		:verbose => false
+	}
+}
 
 begin
 	HTMLProofer.check_directory('./public',options).run
