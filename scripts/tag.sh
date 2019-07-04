f2641d7f6a3ae441914af592fde99bb392216575
Will Patera <willpatera@gmail.com>
2019-07-04 11:27:25 +0700

    Ignore the logo svg for version suffixing

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -12,7 +12,7 @@
 ################################################################################
 variables_func() {
   BRANCHES="master"
-  EXCLUDE="logo.png|navbar.png|\.ico"
+  EXCLUDE="logo.png|navbar.png|\.ico|pl_logo.svg"
   OUTFILE="config-tags.toml"
   PATTERN=".md|.jpg|.jpeg|.pdf|.svg|.gif|.mp4|.webm|.webp"
   SOURCE="config.toml"
